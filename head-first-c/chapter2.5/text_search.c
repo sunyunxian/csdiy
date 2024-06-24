794af598dbf6ac2bcd181dd9b3bcc3c09a2be5e0
Jeff Sun <personal_sunyunxian@foxmail.com>
2024-06-24 16:38:50 +0800

    learn chapter2.5.

---
+++ head-first-c/chapter2.5/text_search.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+    "I left my heart in Harvard Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima",
+};
+
+void find_track(char search_for[]) {
+    for (int i = 0; i < 5; i++) {
+        if (strstr(tracks[i], search_for)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    char search_for[80];
+    printf("Search for: \n");
+    fgets(search_for, 80, stdin);
+    search_for[strlen(search_for) - 1] = '\0';
+    find_track(search_for);
+    return 0;
+}
